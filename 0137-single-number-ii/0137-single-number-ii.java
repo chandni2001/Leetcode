34c5732c37b80d99675d583a179115dbfc9f5548
chandni2001 <88300301+chandni2001@users.noreply.github.com>
2023-07-08 11:56:04 +0530

    Time: 4 ms (62.75%), Space: 44.6 MB (5.91%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+            int ones = 0, twos = 0;
+        for(int e : nums) {
+            ones = ((ones ^ e) & (~ twos));  
+            twos = ((twos ^ e) & (~ ones)); 
+        }
+        return ones;  
+    }
+    
+}
