696f2fc83554ca029eb57596ababf103d845d0e8
chandni2001 <88300301+chandni2001@users.noreply.github.com>
2023-08-01 20:29:26 +0530

    Time: 6 ms (6.22%), Space: 41.2 MB (5.95%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String interpret(String command) {
+        String str="";
+        for(int i=0;i<command.length();i++){
+            char ch=command.charAt(i);
+            if(ch==')'){
+                continue;
+            }
+            if(ch=='G'){
+                str=str+ch;
+            }
+            else if(ch=='('){
+                if(command.charAt(i+1)==')'){
+                      str=str+'o';
+                }
+                
+                
+            }
+            else if(ch=='a'){
+                str=str+ch;
+            }
+            else if(ch=='l'){
+                str=str+ch;
+            }
+        }
+        return str;
+    }
+}
