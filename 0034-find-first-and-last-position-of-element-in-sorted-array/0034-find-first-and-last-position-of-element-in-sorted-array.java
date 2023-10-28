39108a2028e98356bd399e15701be6f461e1f07b
chandni2001 <88300301+chandni2001@users.noreply.github.com>
2023-10-28 09:53:46 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (21.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] a = new int[2];
-        int s =0;
-        int end = nums.length-1;
-        int fi =-1;
-        for(int i=0;i<2;){
-           while(s<=end){
-              int mid = (s+end)/2;
-              if(target<nums[mid]){
-                 end = mid-1;
-              }
-            
-              else if(target>nums[mid]){
-                  s=mid+1;
-              }
-              else{
-                  fi=mid;
-                  end=mid-1;
-                
-              }
-            
-           }
-           a[i]=fi; 
-           i++;
-           s =0;
-           end = nums.length-1;
-           int li = -1;
-           while(s<=end){
-              int mid = (s+end)/2;
-              if(target<nums[mid]){
-                 end = mid-1;
-              }
-            
-              else if(target>nums[mid]){
-                  s=mid+1;
-              }
-              else{
-                   li=mid;
-                   s=s+1;
-                    
-              }
-            
-            }
-            a[i]=li;
-            return a;
-        
-    }
-    int[] ar ={-1,-1};   
-    return ar;   
+        int arr[] ={-1,-1};
+        int a =search(nums,target,true);
+        int b = search(nums,target,false);
+        arr[0]=a;
+        arr[1]=b;
+        return arr;
 }
+    int search(int[]nums,int target,boolean startindex){
+        int ans =-1;
+        int s =0;
+        int e =nums.length-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(target<nums[mid]){
+                e=mid-1;
+            }
+            else if(target>nums[mid]){
+                s=mid+1;
+            }
+            else{
+                ans =mid;
+                if(startindex){
+                    e=mid-1;
+                    
+                }else{
+                    s=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
 }    
