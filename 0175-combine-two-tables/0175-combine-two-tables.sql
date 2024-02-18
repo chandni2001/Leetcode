288fcadd9683c0f768cb36e16a12ea0db0441c54
chandni2001 <88300301+chandni2001@users.noreply.github.com>
2024-02-18 20:32:29 +0530

    Time: 617 ms (98.75%), Space: 0B (100.00%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select Person.firstName, Person.lastName, Address.city, Address.state from Person left join Address on Person.personId = Address.personId;
